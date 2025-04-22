e512bea97f870050462277e3176ad38c1221bf7c
m4rcin7 <mrku1337@gmail.com>
2025-04-22 10:52:32 +0200

    "imp"

--- colors_palette.php
+++ colors_palette.php
@@ -3,9 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>palette of colors</title>
+    <title>Palette of colors</title>
 </head>
 <body>
-    
+    <div class="wrapper">
+        <h1 class="main-title">Color generator</h1>
+        <div class="palette-container">
+            <?php 
+            for ($i =0; $i < 10; $i++) {
+                $randomValueHexStr = dechex(mt_rand(0, 0xFFFFFF));
+                $hexColor = "#" . str_pad($randomValueHexStr, 6, "0", STR_PAD_LEFT);
+            }
+
+            echo "<div class ='color-card'>";
+            echo "  <div class ='color-display' style='background-color: {$hexColor};'>  </div>";
+            echo "  <p> class ='color-card'</p>";
+            echo "</div>";
+            ?>
+        </div>
+        <div class="controls">
+            <div id="btnGenerate" onClick ="window.location.reload()" >New Colors</div>
+        </div>
+    </div>
 </body>
 </html>
