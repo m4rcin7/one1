f114bdff1b0cb5643b234276a7dff5227c4e2fea
m4rcin7 <mrku1337@gmail.com>
2025-04-23 10:35:58 +0200

    :color-generator:

--- colors_palette.php
+++ colors_palette.php
@@ -4,25 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Palette of colors</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="wrapper">
         <h1 class="main-title">Color generator</h1>
         <div class="palette-container">
-            <?php 
-            for ($i =0; $i < 10; $i++) {
-                $randomValueHexStr = dechex(mt_rand(0, 0xFFFFFF));
-                $hexColor = "#" . str_pad($randomValueHexStr, 6, "0", STR_PAD_LEFT);
-            }
+        <?php 
+        for ($i = 0; $i < 10; $i++) {
+            $randomValueHexStr = dechex(mt_rand(0, 0xFFFFFF));
+            $hexColor = "#" . str_pad($randomValueHexStr, 6, "0", STR_PAD_LEFT);
 
-            echo "<div class ='color-card'>";
-            echo "  <div class ='color-display' style='background-color: {$hexColor};'>  </div>";
-            echo "  <p class ='color-card'>{$hexColor}</p>";
-            echo "</div>";
-            ?>
+            echo "<div class='color-card'>
+                    <div class='color-display' style='background-color: {$hexColor};'>{$hexColor}</div>
+                    <p>{$hexColor}</p>
+                    </div>";
+        }
+        ?>
         </div>
         <div class="controls">
-            <div id="btnGenerate" onClick ="window.location.reload()" >New Colors</div>
+        <button id="btnGenerate" onClick="window.location.reload()">New Colors</button>
         </div>
     </div>
 </body>
